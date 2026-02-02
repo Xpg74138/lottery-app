6caed87544251642f347992a7fb2af5645e027a8
Xpg12138 <1099477816@qq.com>
2026-02-02 14:13:03 +0800

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // 使用相对路径
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -12,13 +12,27 @@ export default defineConfig({
     cssCodeSplit: true,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['vue', 'vue-router']
-        },
-        format: 'es',
-        interop: 'auto',
-        preserveModules: false
+        // 不使用 manualChunks，让所有代码打包到一个文件中
+        inlineDynamicImports: false,
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name)) {
+            return 'assets/images/[name]-[hash][extname]'
+          }
+          if (/\.(css)$/i.test(assetInfo.name)) {
+            return 'assets/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
   },
   server: {
